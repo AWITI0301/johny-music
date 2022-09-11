a99f3be841af041ab6ba69cdef4be2260b1a2d67
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-11 21:21:56 +0300

    import other components into audio list

---
+++ src/Components/AudioList.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from "react";
+import { FaHeadphones, FaRegClock, FaRegHeart, FaHeart } from "react-icons/fa";
+import "../Styles/LeftMenu.css";
+import MusicPlayer from "./MusicPlayer";
+import { Songs } from "./Songs";
