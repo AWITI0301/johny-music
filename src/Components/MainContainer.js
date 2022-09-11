e826e4c7961f7d09587d4dcaa80e03512021b67c
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-11 20:25:12 +0300

    styles right container

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Styles/MainContainer.css'
 
 function MainContainer() {
   return (
--- src/Components/RightMenu.js
+++ src/Components/RightMenu.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Styles/RightMenu.css'
 
 function RightMenu() {
   return (
