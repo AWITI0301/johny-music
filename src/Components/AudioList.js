d947b2f291abb5ac99867b48af8ee9457e0dc181
AWITI0301 <johnoketchawiti@gmail.com>
2022-09-11 21:28:56 +0300

    create variable song,image and set auto in the audiolist

--- src/Components/AudioList.js
+++ src/Components/AudioList.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { FaHeadphones, FaRegClock, FaRegHeart, FaHeart } from "react-icons/fa";
 import "../Styles/LeftMenu.css";
-import MusicPlayer from "./MusicPlayer";
+// import MusicPlayer from "./MusicPlayer";
 import { Songs } from "./Songs";
+
+function AudioList() {
+    const [songs, setSongs] = useState(Songs);
+    const [song, setSong] = useState(songs[0].song);
+    const [img, setImage] = useState(songs[0].imgSrc);
+    const [auto, setAuto] = useState(false);
+}
